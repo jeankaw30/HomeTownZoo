1fd429a8cc0559f60b7cfff6063c0745a7fb087b
JEANNA KAWAMOTO <kawzie30@live.com>
2018-09-24 14:24:38 -0700

    Add project files.

---
+++ HomeTownZoo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeTownZoo.Startup))]
+namespace HomeTownZoo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
