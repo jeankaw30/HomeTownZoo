e6dcb1155f8dd1aa52a1cdec051617a02a3d19be
JEANNA KAWAMOTO <kawzie30@live.com>
2018-10-03 13:34:58 -0700

    modified password and registration criteria to be easy for class purposes

--- HomeTownZoo/App_Start/IdentityConfig.cs
+++ HomeTownZoo/App_Start/IdentityConfig.cs
@@ -46,18 +46,19 @@ namespace HomeTownZoo
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
-                AllowOnlyAlphanumericUserNames = false,
-                RequireUniqueEmail = true
+                AllowOnlyAlphanumericUserNames = true,
+                RequireUniqueEmail = false
             };
 
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                // change back to true to implement restriction
+                RequiredLength = 3,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
