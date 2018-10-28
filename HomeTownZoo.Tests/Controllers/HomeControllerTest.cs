4bff19a897b55a6d7b4cbbbc3476931ceeeee512
JEANNA KAWAMOTO <kawzie30@live.com>
2018-10-28 11:55:42 -0700

    Catching up on lectures  changes up to Teesting databases beginning of lecure Created tests  for home controller action results in test  project Configured Entity Framework package to test project Configured Moq package to test project Created Animals view Created Animals controller scaffolded Created AnimalServices test project model Created tests for Animals services

--- HomeTownZoo.Tests/Controllers/HomeControllerTest.cs
+++ HomeTownZoo.Tests/Controllers/HomeControllerTest.cs
@@ -10,45 +10,61 @@ using HomeTownZoo.Controllers;
 namespace HomeTownZoo.Tests.Controllers
 {
     [TestClass]
-    public class HomeControllerTest
+    public class HomeControllerTest // testing home controller
     {
-        [TestMethod]
-        public void Index()
+        [TestMethod] // #1 tests home controllers index action
+        public void Index_ReturnsNonNullViewResults()
         {
-            // Arrange
+            // ARRANGE
             HomeController controller = new HomeController();
 
-            // Act
-            ViewResult result = controller.Index() as ViewResult;
+            // ACT by calling the method we want to test
+            ViewResult result = controller.Index() as ViewResult;   // call controller Index action result method
+                                                                    // returns data type ViewResult with result as new variable
+            // ASSERT 
+            Assert.IsNotNull(result);
+ 
+        }
 
-            // Assert
+        [TestMethod] // #2 tests home controllers about action
+        public void About_ReturnsNonNullViewResult()
+        {
+            // ARRANGE
+            HomeController home = new HomeController();
+
+            // ACT by calling the method we want to test
+            ViewResult result = home.About() as ViewResult; // call controller Index action result method
+                                                                  // returns data type ViewResult with result as new variable
+            // ASSERT 
             Assert.IsNotNull(result);
         }
 
-        [TestMethod]
-        public void About()
+
+        [TestMethod] // #3
+        public void About_ShouldHaveViewBagMessage()
         {
-            // Arrange
-            HomeController controller = new HomeController();
+            // ACT
+            HomeController home = new HomeController();
 
-            // Act
-            ViewResult result = controller.About() as ViewResult;
+            // ACT by calling the method we want to test
+            ViewResult result = home.About() as ViewResult;
 
-            // Assert
+            // ASSERT
+            Assert.IsNotNull(result.ViewBag.Message);
+           
+            // OR check that viewbag string matches specific string message
             Assert.AreEqual("About Hometown Zoo", result.ViewBag.Message);
+            
+            // OR 
+            Assert.AreNotEqual(string.Empty, ((string)result.ViewBag.Message).Trim());
+            
+            // OR ASSERT USING A CREATED VARIABLE
+            string message = result.ViewBag.Message;
+            Assert.IsNotNull(message);
+            Assert.AreNotEqual(string.Empty, message.Trim());
         }
 
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
 
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
 
-            // Assert
-            Assert.IsNotNull(result);
-        }
     }
 }
---
+++ HomeTownZoo/Models/AnimalService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeTownZoo.Models
+{
+    public static class AnimalService
+    {
+        /// <summary>
+        /// Returns all Animals by Name sorted in Ascending order from database
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public static IEnumerable<Animal> GetAnimals(ApplicationDbContext db)
+        {
+            //query sntax
+            return (from a in db.Animals
+                    orderby a.AnimalName
+                    select a).ToList();
+
+            // method syntax
+            //return db.Animals
+            //        .OrderBy(a => a.AnimalName)
+            //        .ToList();
+        }
+
+    }
+}
