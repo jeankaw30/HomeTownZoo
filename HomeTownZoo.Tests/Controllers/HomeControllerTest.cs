d881b22b9fa124e18436b1d0af08c055012036d9
JEANNA KAWAMOTO <kawzie30@live.com>
2018-10-27 21:11:21 -0700

    Changed homeController test viewbag message to match  about controller page method ain order for the about test   to pass

--- HomeTownZoo.Tests/Controllers/HomeControllerTest.cs
+++ HomeTownZoo.Tests/Controllers/HomeControllerTest.cs
@@ -35,7 +35,7 @@ namespace HomeTownZoo.Tests.Controllers
             ViewResult result = controller.About() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+            Assert.AreEqual("About Hometown Zoo", result.ViewBag.Message);
         }
 
         [TestMethod]
